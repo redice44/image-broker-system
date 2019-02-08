78d86600a63e28a5aad15fcc01ec98bcb4b7ca96
Matt Thomson <matt.thomson@sharpnotions.com>
2019-02-08 10:54:44 -0500

    Send image to image store

--- image-source/src/index.js
+++ image-source/src/index.js
@@ -1,16 +1,26 @@
+const AWS = require('aws-sdk');
 const fs = require('fs');
 const rp = require('request-promise');
 const moment = require('moment');
 const mkdirp = require('mkdirp');
 const TopicConnector = require('@redice44/rabbitmq-topic-routing-schema');
 const topic = require('./topics/images');
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  endpoint: process.env.AWS_ENDPOINT,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4'
+});
+const bucket = process.env.S3_BUCKET;
+const IMG_EXT = 'jpeg';
 
 const main = async () => {
   const ms = process.env.POLL_INTERVAL;
   const url = process.env.IMAGE_SOURCE;
   const filePath = process.env.IMAGE_DIR;
 
-  mkdirp(filePath);
   const connectionString = {
     user: process.env.RABBITMQ_USER,
     pass: process.env.RABBITMQ_PASS,
@@ -18,28 +28,72 @@ const main = async () => {
   };
   const { name, schema } = topic;
   const connection = await setupConnection(connectionString, { name, schema });
+
+  mkdirp(filePath);
+
+  try {
+    const headBucket = await s3.headBucket({
+      Bucket: bucket
+    }).promise();
+  } catch (error) {
+    if (error.code && error.code === 'NotFound') {
+      const res = await s3.createBucket({
+        Bucket: bucket
+      }).promise();
+    } else {
+      process.exit(1);
+    }
+  }
+
   setInterval(getImage, ms, connection, url, filePath);
+  //const fileName = await getImage(connection, url, filePath);
+  //await retrieveImage(fileName, filePath);
+  //process.exit(0);
+};
+
+const retrieveImage = async (key, filePath) => {
+  const fileLocation = `${filePath}/minio-${key}`;
+  const file = await s3.getObject({
+    Bucket: bucket,
+    Key: key,
+  }).promise();
+  const buff = Buffer.from(file.Body);
+  fs.writeFileSync(fileLocation, buff, { encoding: 'binary' });
 };
 
 const getImage = async (brokerConnection, url, filePath) => {
-  const fileLocation = `${filePath}/${moment().format('x')}.jpeg`;
-  const img = await rp.get({ url, encoding: 'binary' });
+  const fileName = `${moment().format('x')}.${IMG_EXT}`;
+  //const fileLocation = `${filePath}/${fileName}`;
+  console.log(`=== ${fileName} ===`);
+  console.log('Capturing image');
+  const img = await rp.get({ url, encoding: null });
   const exchangeTopic = {
     location: 'canalParkingLot',
     sourceType: 'raw',
     format: 'jpeg'
   };
   const brokerMessage = {
-    imageLocation: fileLocation
+    bucket,
+    key: fileName
   };
-
-  console.log(`Writing ${fileLocation}...`);
-  fs.writeFileSync(fileLocation, img, { encoding: 'binary' });
-  await brokerConnection.publishToTopic(
-    exchangeTopic,
-    JSON.stringify(brokerMessage),
-    { timestamp: +moment() }
-  );
+  //fs.writeFileSync(fileLocation, img, { encoding: 'base64' });
+  try {
+    console.log('Storing image');
+    const r = await s3.putObject({
+      Bucket: bucket,
+      Key: fileName,
+      Body: img
+    }).promise();
+    console.log('Publishing event');
+    await brokerConnection.publishToTopic(
+      exchangeTopic,
+      JSON.stringify(brokerMessage),
+      { timestamp: +moment() }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  return fileName;
 };
 
 const setupConnection = async (connectionString, { name, schema }) => {
